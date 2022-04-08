b90065e15eecc73d5f761a5499b8a63860ae882c
Ege DURMAZ <ege_durmaz09@hotmail.com>
2022-04-08 14:55:15 +0300

    0.4

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Gamelogic } from '../gamelogic';
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
+  providers:[Gamelogic]
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor( public game: Gamelogic) { }
 
   ngOnInit(): void {
   }
 
+  startGame(): void {
+    this.game.gameStart();
+    const currentPlayer = 'Current turn: Player: '+this.game.currentTurn;
+    const inf=document.querySelector(".current-status");
+    if(inf!=null){
+      inf.innerHTML=currentPlayer;
+    }
+  }
+
+  async clickSubfield(subfield:any): Promise<void>{
+    if(this.game.gameStatus===1){
+      const position = subfield.currentTarget.getAttribute('position');
+      
+    }
+  }
 }
