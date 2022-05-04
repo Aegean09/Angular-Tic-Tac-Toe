23c9d794740f5a605894ff72b3b5ce511900a702
Ege DURMAZ <ege_durmaz09@hotmail.com>
2022-05-04 17:49:48 +0300

    v1.1 (Added "Against AI" Mode)

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import { promisify } from 'util';
 import { Gamelogic } from '../gamelogic';
 
 
@@ -13,6 +14,30 @@ export class GameComponent implements OnInit {
   constructor( public game: Gamelogic) { }
 
   ngOnInit(): void {
+  }
+  
+  startAI():void{
+    this.game.AIStart();
+    var currentsymbol;
+    if (this.game.currentTurn==1){
+      currentsymbol="X";
+    }
+    else{
+      currentsymbol="O";
+    }
+    const inf=document.querySelector(".current-status");
+    const currentPlayer = 'It\'s your turn...';
+    if(inf!=null){
+        inf.innerHTML=currentPlayer;
+     }
+    const start_button1=document.getElementById("start_button");
+    if(start_button1!=null){
+      start_button1.style.display="none";
+    }
+    const start_ai_button1=document.getElementById("start_ai");
+    if(start_ai_button1!=null){
+      start_ai_button1.style.display="none";
+    }
   }
   startGame(): void {
     this.game.gameStart();
@@ -32,10 +57,184 @@ export class GameComponent implements OnInit {
     if(start_button1!=null){
       start_button1.style.display="none";
     }
+    const start_ai_button1=document.getElementById("start_ai");
+    if(start_ai_button1!=null){
+      start_ai_button1.style.display="none";
+    }
   }
 
   async clickSubfield(subfield:any): Promise<void>{
     var currentsymbol;
+
+    //#region Against AI
+    if(this.game.gameStatus===2){
+      const position = subfield.currentTarget.getAttribute('position');
+      const inf=document.querySelector(".current-status");
+
+      if(this.game.gameStatus===2){
+        const currentPlayer = 'It\'s your turn...';
+        if(inf!=null){
+          inf.innerHTML=currentPlayer;
+        }
+      }
+      if(this.game.currentTurn==1){
+        if(await this.game.checkEmpty(position)){
+          if(await this.game.checkEmpty(position)){
+            this.game.setField(position, this.game.currentTurn);
+            const color=this.game.getPlayerColorClass();
+            subfield.currentTarget.classList.add(color);
+          }
+          await this.game.checkGameEndWinner().then( (end: boolean)=>{
+            if(this.game.gameStatus===0 && end){
+              if (inf!=null){
+                if (this.game.currentTurn==1){
+                  currentsymbol="X";
+                }
+                else{
+                  currentsymbol="O";
+                }
+                inf.innerHTML="The winner is player: "+currentsymbol;
+                const start_button1=document.getElementById("start_button");
+                if(start_button1!=null){
+                  start_button1.style.display="block";
+                }
+                const start_ai_button1=document.getElementById("start_ai");
+                if(start_ai_button1!=null){
+                  start_ai_button1.style.display="block";
+                }
+              }
+            }
+             this.game.checkGameEndFull().then( (end: number)=>{
+              if(this.game.gameStatus===0 && end==1){
+                if (inf!=null){
+                  inf.innerHTML="No Winner, DRAW!";
+                  const start_button1=document.getElementById("start_button");
+                  if(start_button1!=null){
+                    start_button1.style.display="block";
+                  }
+                  const start_ai_button1=document.getElementById("start_ai");
+                  if(start_ai_button1!=null){
+                    start_ai_button1.style.display="block";
+                  }
+                }
+              }
+              if(this.game.gameStatus===0 && end==0){
+                if (inf!=null){
+                  if (this.game.currentTurn==1){
+                    currentsymbol="X";
+                  }
+                  else{
+                    currentsymbol="O";
+                  }
+                  inf.innerHTML="The winner is player: "+currentsymbol;
+                  const start_button1=document.getElementById("start_button");
+                  if(start_button1!=null){
+                    start_button1.style.display="block";
+                  }
+                  const start_ai_button1=document.getElementById("start_ai");
+                  if(start_ai_button1!=null){
+                    start_ai_button1.style.display="block";
+                  }
+                }
+              }
+            });
+          });
+
+          //#region AI
+          
+          this.game.currentTurn=2;
+          if(this.game.gameStatus===2){
+            const currentPlayer = 'I\'m thinking.... ';
+            if(inf!=null){
+              inf.innerHTML=currentPlayer;
+            }
+          }
+          await new Promise(f=>setTimeout(f,2000));
+          var rand_index = this.game.changeAI();
+          while(await this.game.checkEmpty(rand_index)==false){
+            rand_index=this.game.changeAI();
+            console.log(rand_index);
+          }
+          if(this.game.currentTurn==2){
+            if(await this.game.checkEmpty(rand_index)){
+              if(await this.game.checkEmpty(rand_index)){
+                this.game.setField(rand_index, this.game.currentTurn);
+                const color_ai=this.game.getPlayerColorClass();
+                document.getElementById(rand_index.toString())?.classList.add(color_ai);
+              }
+              await this.game.checkGameEndWinner().then( (end: boolean)=>{
+                if(this.game.gameStatus===0 && end){
+                  if (inf!=null){
+                    if (this.game.currentTurn==1){
+                      currentsymbol="X";
+                    }
+                    else{
+                      currentsymbol="O";
+                    }
+                    inf.innerHTML="The winner is player: "+currentsymbol;
+                    const start_button1=document.getElementById("start_button");
+                    if(start_button1!=null){
+                      start_button1.style.display="block";
+                    }
+                    const start_ai_button1=document.getElementById("start_ai");
+                    if(start_ai_button1!=null){
+                      start_ai_button1.style.display="block";
+                    }
+                  }
+                }
+                 this.game.checkGameEndFull().then( (end: number)=>{
+                  if(this.game.gameStatus===0 && end==1){
+                    if (inf!=null){
+                      inf.innerHTML="No Winner, DRAW!";
+                      const start_button1=document.getElementById("start_button");
+                      if(start_button1!=null){
+                        start_button1.style.display="block";
+                      }
+                      const start_ai_button1=document.getElementById("start_ai");
+                      if(start_ai_button1!=null){
+                        start_ai_button1.style.display="block";
+                      }
+                    }
+                  }
+                  if(this.game.gameStatus===0 && end==0){
+                    if (inf!=null){
+                      if (this.game.currentTurn==1){
+                        currentsymbol="X";
+                      }
+                      else{
+                        currentsymbol="O";
+                      }
+                      inf.innerHTML="The winner is player: "+currentsymbol;
+                      const start_button1=document.getElementById("start_button");
+                      if(start_button1!=null){
+                        start_button1.style.display="block";
+                      }
+                      const start_ai_button1=document.getElementById("start_ai");
+                      if(start_ai_button1!=null){
+                        start_ai_button1.style.display="block";
+                      }
+                    }
+                  }
+                });
+              });
+            }
+          }
+          //#endregion
+
+          if(this.game.gameStatus===2){
+            this.game.changePlayer();
+            const currentPlayer = 'It\'s your turn1...'+this.game.gameStatus;
+            if(inf!=null){
+                inf.innerHTML=currentPlayer;
+            }
+          }          
+        }
+      }
+    }
+    //#endregion
+
+
+    //#region Co-Op
     if(this.game.gameStatus===1){
       const position = subfield.currentTarget.getAttribute('position');
       const inf=document.querySelector(".current-status");
@@ -59,6 +258,10 @@ export class GameComponent implements OnInit {
               if(start_button1!=null){
                 start_button1.style.display="block";
               }
+              const start_ai_button1=document.getElementById("start_ai");
+              if(start_ai_button1!=null){
+                start_ai_button1.style.display="block";
+              }
             }
           }
            this.game.checkGameEndFull().then( (end: number)=>{
@@ -68,6 +271,10 @@ export class GameComponent implements OnInit {
                 const start_button1=document.getElementById("start_button");
                 if(start_button1!=null){
                   start_button1.style.display="block";
+                }
+                const start_ai_button1=document.getElementById("start_ai");
+                if(start_ai_button1!=null){
+                  start_ai_button1.style.display="block";
                 }
               }
             }
@@ -83,6 +290,10 @@ export class GameComponent implements OnInit {
                 const start_button1=document.getElementById("start_button");
                 if(start_button1!=null){
                   start_button1.style.display="block";
+                }
+                const start_ai_button1=document.getElementById("start_ai");
+                if(start_ai_button1!=null){
+                  start_ai_button1.style.display="block";
                 }
               }
             }
@@ -103,5 +314,6 @@ export class GameComponent implements OnInit {
         }
       }
     }
+    //#endregion
   }
 }
